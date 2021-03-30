c45f792c9be35990c65ea8874bad117a0576ecb0
Nikita Behmetev <nikitabehmetev@gmail.com>
2021-03-30 09:29:04 +0300

    не большие правки

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?
-$id_channel = 'UC1nxTJHKHTh_Wc6cbMle0qg';
-$api_key = 'AIzaSyDKHPLBYUxePuG2yKgiRzYKIVX4_0qKdzw';
 
-$logfile = $_SERVER["DOCUMENT_ROOT"]."/dev/youtube-pars/log.txt";
-
+$id_channel = 'my_id_canal';
+$api_key = 'my_api_key';
+$logfile = "/logs/log.txt";
 $arrVideoID = [];
 
 if (CModule::IncludeModule("iblock")){
     $res = CIBlockElement::GetList(Array(), Array("IBLOCK_ID"=>11));
     while($ob = $res->GetNext()){
-        //print_r($ob["ID"]); получвем id всех элементов раздела
         $res2 = CIBlockElement::GetProperty(11, $ob["ID"], array("sort" => "asc"), Array("CODE"=>"KEY"));
         while ($ob2 = $res2->GetNext()) {
             $arrVideoID[] = $ob2['VALUE'];
@@ -21,13 +19,10 @@ if (CModule::IncludeModule("iblock")){
 $json_result = file_get_contents ("https://youtube.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=$id_channel&maxResults=5&key=$api_key");
 $obj = json_decode($json_result, true);
 
-//echo $obj['items'][0]['contentDetails']['upload']['videoId'];
-
 if (!in_array($obj['items'][0]['contentDetails']['upload']['videoId'], $arrVideoID)) {
-    //echo "есть новые видео";
 
     $el = new CIBlockElement;
-
+    // app props data
     $PROP = array();
     $PROP[96] = $obj['items'][0]['contentDetails']['upload']['videoId'];
     $PROP[106] = "youtube";
@@ -38,13 +33,13 @@ if (!in_array($obj['items'][0]['contentDetails']['upload']['videoId'], $arrVideo
     $PROP[112] = $obj['items'][0]['snippet']['description'];
 
     $arLoadProductArray = Array(
-        "MODIFIED_BY"    => 29574, // элемент изменен пользователем Бехметьев Никита
+        "MODIFIED_BY"    => 29574, //id user
         "IBLOCK_SECTION_ID" => false,
         "DATE_ACTIVE_FROM" => date("j.n.Y"),
         "IBLOCK_ID"      => 11,
         "PROPERTY_VALUES"=> $PROP,
         "NAME"           => $obj['items'][0]['snippet']['title'],
-        "ACTIVE"         => "Y",            // активен
+        "ACTIVE"         => "Y",
         "PREVIEW_TEXT"   => $obj['items'][0]['snippet']['description'],
         "DETAIL_TEXT"    => "",
         "DETAIL_PICTURE" => CFile::MakeFileArray($obj['items'][0]['snippet']['thumbnails']['standard']['url'])
@@ -68,16 +63,4 @@ if (!in_array($obj['items'][0]['contentDetails']['upload']['videoId'], $arrVideo
     fwrite($fw, "\n-------------\n");
     fclose($fw);
 }
-/*
-foreach ($obj['items'] as &$value) {
-    //print_r($value);
-    echo $value['snippet']['title'];
-    echo "<br>";
-    echo $value['snippet']['description'];
-    echo "<br>";
-    echo $value['snippet']['thumbnails']['standard']['url'];
-    echo "<br>";
-    echo $value['contentDetails']['upload']['videoId'];
-    echo "<br>---<br>";
-}*/
 ?>
